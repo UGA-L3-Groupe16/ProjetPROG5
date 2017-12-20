5d5f3721a3bc0e250e45f39bd16ebdae7fb9f341
Adrien <adrien.villalta@gmail.com>
2017-12-20 16:13:04 +0100

    ecriture STR/STRB/STRH; factorisation

--- arm_load_store.c
+++ arm_load_store.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T Ã  but pÃ©dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique GÃ©nÃ©rale GNU publiÃ©e par la Free Software
+Foundation (version 2 ou bien toute autre version ultÃ©rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuÃ© car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spÃ©cifique. Reportez-vous Ã  la
+Licence Publique GÃ©nÃ©rale GNU pour plus de dÃ©tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reÃ§u une copie de la Licence Publique GÃ©nÃ©rale GNU en mÃªme
+temps que ce programme ; si ce n'est pas le cas, Ã©crivez Ã  la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+Ã‰tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 BÃ¢timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'HÃ¨res
 */
 #include "arm_load_store.h"
 #include "arm_exception.h"
@@ -26,8 +26,162 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 
+uint32_t Op_bit_U(uint32_t address, uint16_t offset, uint32_t ins){	
+//Signe de l'offset
+
+	if(get_bit(ins, 23)){
+		return address + offset;
+	} else {
+		return address - offset;
+	}
+}
+
+void Op_bit_I(arm_core p, uint32_t address, uint16_t offset, uint32_t ins){	
+//Offset immediat ou via registre, pour STR/STRB
+
+	uint8_t reg_offset;		//Rm
+	
+	if(get_bit(ins, 25)){
+	//Registre
+
+		reg_offset = get_bits(ins, 3, 0);
+		offset = read_register(p->reg, reg_offset);
+		address = Op_bit_U(address, offset, ins);
+	} else {
+	//Immediat
+
+		offset = get_bits(ins, 11, 0);
+		address = Op_bit_U(address, offset, ins);
+	}
+}
+
+void Op_bit_22(arm_core p, uint32_t address, uint16_t offset, uint32_t ins){	
+//Offset immediat ou via registre, pour STRH
+	
+	uint8_t immedH;			//haut de l'offset
+	uint8_t immedL;			//bas de l'offset
+	uint8_t reg_offset;		//Rm
+	
+	if (get_bit(ins, 22)){
+	//Immediat
+
+		immedH = get_bits(ins, 11, 8);
+		immedL = get_bits(ins, 3, 0);
+		offset = (immedH << 4) | immedL;
+		Op_bit_U(address, offset, ins);
+	} else {
+	//Registre
+
+		reg_offset = get_bits(ins, 3, 0);
+		offset = read_register(p->reg, reg_offset);
+		address = Op_bit_U(address, offset, ins);
+	}
+}
+
+void Op_bit_W(arm_core p, uint32_t address, uint16_t offset, uint32_t ins, uint8_t dest){
+//Ecriture de la nouvelle adresse dans le registre ou pas
+
+	if (get_bit(ins, 26)){
+		Op_bit_I(p, address, offset, ins);
+	} else {
+		Op_bit_22(p, address, offset, ins);
+	}
+	if (get_bit(ins, 21)){
+	//Pre-indexed addressing -> enregistrement dans le registre
+
+		write_register(p->reg, dest, address);
+	}
+}
+
+int store(arm_core p , uint32_t ins, uint8_t source, uint8_t dest, uint32_t address){
+	uint32_t value;			//Valeur stockÃ©e, word
+	uint16_t value_half;	//Valeur stockÃ©e, half-word
+	uint8_t value_byte;		//Valeur stockÃ©e, byte
+	int success;
+
+	if (get_bit(ins, 26){
+		//STR / STRB
+
+		if (get_bit(ins, 22)){		
+		//STRB
+
+			value_byte = (read_register(p->reg, source)) & 0xFF;
+		} else {	
+		//STR
+
+			value = read_register(p->reg, source);
+		}
+		if (get_bit(ins, 24)){	
+		//Pre-indexed addressing / offset addressing
+
+			Op_bit_W(p, address, offset, ins, dest);
+			if (get_bit(ins, 22)){
+			//STRB
+
+				success = memory_write_byte(p->mem, address, value_byte);
+			} else {	
+			//STR
+
+				success = memory_write_word(p->mem, address, value);
+			}
+		} else {	
+		//Post-indexed addressing
+
+			if (get_bit(ins, 22)){		
+			//STRB
+
+				success = memory_write_byte(p->mem, address, value_byte);
+			} else {	
+			//STR
+
+				success = memory_write_word(p->mem, address, value);
+			}
+			Op_bit_I(p, address, offset, ins);
+			write_register(p->reg, dest, address);					
+		}
+	} else {	
+	//STRH
+
+		value_half = (read_register(p->reg, source)) & 0xFFFF;
+		if (get_bit(ins, 24)){	
+		//Pre-indexed addressing / offset addressing
+
+			Op_bit_W(p, address, offset, ins, dest);
+			success = memory_write_half(p->mem, address, value_half);
+		} else {	
+		//Post-indexed addressing		
+
+			success = memory_write_half(p->mem, address, value_half);
+			Op_bit_22(p, address, offset, ins);
+			write_register(p->reg, dest, address);
+		}
+	}
+	return success;
+}
+
 int arm_load_store(arm_core p, uint32_t ins) {
-    return UNDEFINED_INSTRUCTION;
+
+	uint32_t value;			//Valeur stockÃ©e, word
+	uint16_t value_half;	//Valeur stockÃ©e, half-word
+	uint8_t value_byte;		//Valeur stockÃ©e, byte
+	uint8_t source;			//Rn
+	uint8_t dest;			//Rd
+	uint32_t address;
+	int success;
+
+	source = get_bits(ins, 19, 16);
+	dest = get_bits(ins, 15, 12);
+	address = read_register(p->reg, dest);
+	if (get_bit(ins, 20)){	
+	//LOAD
+
+		success = load(p, ins, source, dest, address);
+	} else {
+	//STORE
+
+		success = store(p, ins, source, dest, address);
+	}
+    return success;
 }
 
 int arm_load_store_multiple(arm_core p, uint32_t ins) {
