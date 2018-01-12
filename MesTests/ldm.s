1afe1f5f29dd312ddcdde3efa8916a1c5672d1bd
Eminaelle <coraliedurhone@gmail.com>
2018-01-12 14:06:05 +0100

    test modifie

--- MesTests/ldm.s
+++ MesTests/ldm.s
@@ -3,16 +3,24 @@
 main:
     mov r4, #1
     ldr r1, =donnee
+
+    @mets la valeur de ladresse 0X2804 dans r5
     ldr r5, [r1, +r4, lsl #2]
+
+    @mets la valeur des adresses 0X2800-0X280C dans r6, r7 et r9, avec une mise a jour de r1
     ldmia r1!, {r6, r7, r9}
     ldr r1, =donnee
+    @mets la valeur des adresses 0X2800-0X208C dans r6, r7 et r9, sans une mise a jour de r1
     ldmia r1, {r6, r7, r9}
     ldr r1, =donnee
+    @mets la valeur des adresses 0X2804-0X2810 dans r6, r7 et r9, avec une mise a jour de r1
     ldmib r1!, {r6, r7, r9}
     ldr r1, =donnee
+    @mets la valeur des adresses 0X2804-0X2810 dans r6, r7 et r9, sans une mise a jour de r1
     ldmib r1, {r6, r7, r9}
     ldr r1, =fin
     add r1, r1, #4
+    @mets la valeur des adresses 0X2804-0X2810 dans r6, r7 et r9, sans une mise a jour de r1
     ldmda r1!, {r6, r7, r9}
     ldr r1, =fin
     add r1, r1, #4
